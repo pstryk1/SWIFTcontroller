33efab0eeb961ce6c6444121daa3fccc7198f374
pstryk1 <150193642+pstryk1@users.noreply.github.com>
2025-03-22 16:39:40 +0100

    Initial commit

---
+++ src/main/kotlin/com/PatrykKusper/SWIFTcontroller/SwiftControllerApplication.kt
@@ -0,0 +1,11 @@
+package com.PatrykKusper.SWIFTcontroller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwiftControllerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwiftControllerApplication>(*args)
+}
---
+++ src/test/kotlin/com/PatrykKusper/SWIFTcontroller/SwiftControllerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.PatrykKusper.SWIFTcontroller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwiftControllerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
